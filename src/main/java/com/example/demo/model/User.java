62eeaa24107bee9991d38b31dc2dde351eeb70ac
Nora <poyin311@gmail.com>
2018-09-11 12:10:59 +0800

    update import

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,17 @@
 package com.example.demo.model;
 
-import java.beans.Transient;
+
 import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "user")
@@ -61,6 +69,7 @@ public class User {
         this.roles = roles;
     }
 
+    @Transient
 	public String getTitle() {
 		return title;
 	}

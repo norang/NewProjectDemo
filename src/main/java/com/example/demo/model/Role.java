99dfc627849c971777f299585d6dd39897ba4aff
Nora <poyin311@gmail.com>
2018-09-12 16:55:40 +0800

    role type

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 public class Role {
     private Long id;
     private String name;
+    private String type;
     private Set<User> users;
 
     @Id
@@ -42,4 +43,14 @@ public class Role {
     public void setUsers(Set<User> users) {
         this.users = users;
     }
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+    
+    
 }

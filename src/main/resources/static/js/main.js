49b58e8977cd116cf15da7dab0b7e44ad959f124
Nora <poyin311@gmail.com>
2018-10-09 10:37:52 +0800

    dynamic set active nav item

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -18,3 +18,15 @@ function openUModal(id){
         }  
 	});
 }
+
+$(function () {
+    var url = window.location.pathname; 
+    var activePage = url.substring(url.lastIndexOf('/') + 1); 
+        $('.navbar-nav li a').each(function () { 
+            var linkPage = this.href.substring(this.href.lastIndexOf('/') + 1); 
+ 
+            if (activePage == linkPage) { 
+                $(this).parent().addClass('active'); 
+            }
+        });
+});
